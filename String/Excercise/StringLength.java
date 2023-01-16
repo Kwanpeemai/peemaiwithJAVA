e5a6c5df00e551ba11cb55a66ea22f45ddcd9e97
Kwanpeemai P <110528363+Kwanpeemai@users.noreply.github.com>
2023-01-16 20:46:51 +0700

    finish

---
+++ String/Excercise/StringLength.java
@@ -0,0 +1,11 @@
+package String.Excercise;
+
+import java.util.Scanner;
+
+public class StringLength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        System.out.println(text.length());
+    }
+}
