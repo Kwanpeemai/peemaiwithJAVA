943bc264d84bbee634429e345b08da80fa751741
Kwanpeemai P <110528363+Kwanpeemai@users.noreply.github.com>
2023-03-11 18:07:33 +0700

    finish

--- Class/ClassComposition/AliCarCar1.java
+++ Class/ClassComposition/AliCarCar1.java
@@ -7,44 +7,44 @@ class Car {
     String name;
     int price;
     int labor;
-    Car (int ID, String name, int price, int labor){
-        this.ID = ID;
+
+    Car(String name, int price, int labor) {
         this.name = name;
         this.price = price;
         this.labor = labor;
     }
-    static boolean check (String name, int price, int labor){
-        if (name.length() != 0 && price > 0 && labor > 0) {
+
+    static boolean check(String name, int price, int labor) {
+        if (name != null && name.length() != 0 && price > 0 && labor > 0) {
             return true;
         } else {
-            return false;    
-        }  
+            return false;
+        }
     }
 
 }
 
 public class AliCarCar1 {
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    int N = scan.nextInt();
-    Car[] cars = new Car[N];
-    int count = 0;
-    for(int i = 0; i < N; ++i) {
-      String name = scan.next();
-      int price = scan.nextInt();
-      int labor = scan.nextInt();
-      cars[i] = new Car(i, name, price, labor);
-      if(name.length() != 0 && price > 0 && labor > 0) {
-        cars[i].check(name, price, labor);  
-        System.out.println("valid");
-        count++;
-      } else {
-        System.out.println("invalid");
-      }
-}
-   for(int i=0;i<count;i++){
-        System.out.println(i+1 + " " + cars[i].name);
-   }
-       
-}
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        Car[] cars = new Car[N];
+        int count = 1;
+        for (int i = 0; i < N; ++i) {
+            String name = scan.next();
+            int price = scan.nextInt();
+            int labor = scan.nextInt();
+            if (cars[i].check(name, price, labor) == true) {
+                cars[count] = new Car(name, price, labor);
+                count++;
+                System.out.println("valid");
+            } else {
+                System.out.println("invalid");
+            }
+        }
+        for (int i = 1; i < count; i++) {
+            System.out.println(i + " " + cars[i].name);
+        }
+
+    }
 }
